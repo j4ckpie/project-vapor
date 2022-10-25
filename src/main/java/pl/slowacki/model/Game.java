4c20c15babaaad3ec4324c42533b3785dc7a9042
Jakub Pietrala <pietrala.jakub05@gmail.com>
2022-10-25 22:31:22 +0200

    Added Game class

---
+++ src/main/java/pl/slowacki/model/Game.java
@@ -0,0 +1,85 @@
+package pl.slowacki.model;
+
+public class Game {
+    private String name;
+    private String description;
+    private String developer;
+    private String publisher;
+    private String tag;
+    private String[] reviews;
+    private int yearOfPublication;
+    private double price;
+    private double rating;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDeveloper() {
+        return developer;
+    }
+
+    public void setDeveloper(String developer) {
+        this.developer = developer;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public String[] getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(String[] reviews) {
+        this.reviews = reviews;
+    }
+
+    public int getYearOfPublication() {
+        return yearOfPublication;
+    }
+
+    public void setYearOfPublication(int yearOfPublication) {
+        this.yearOfPublication = yearOfPublication;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+}
